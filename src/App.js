dde8058dee188940fa8e6c025d86bc0dd24e9f7b
nuzulzen <nuzulzen@gmail.com>
2020-11-06 21:59:19 +0700

    fix someting

--- src/App.js
+++ src/App.js
@@ -1,76 +1,69 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Container } from "react-bootstrap";
-import Select from "react-select";
-import { useEffect, useState } from "react";
-import { IntlProvider, FormattedMessage } from "react-intl";
+import { useState } from "react";
+import { Trans, useTranslation } from "react-i18next";
 
 function App() {
-  const options = [
-    { value: "chocolate", label: "Chocolate" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
-  ];
-
-  const [selectedOption, setSelectedOption] = useState(null);
-  const [locale, setLocale] = useState("en");
-  const [messages, setMessage] = useState();
-
-  useEffect(() => {
-    lang(locale, "").then((messages) => {
-      setMessage(messages);
-    });
-  }, []);
-
-  if (!messages) return null;
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
 
   return (
-    <IntlProvider>
-      <div className="App">
-        <img
-          className="login__imgBg"
-          src="https://taskdev.mile.app/d09f7f2de7d88fb5f1575273b8a26426.png"
-          alt=""
-        />
-        <Container className="m4 login__container">
-          <div className="left">
-            <div className="left__content">
-              <Select
-                defaultValue={selectedOption}
-                onChange={setSelectedOption}
-                options={options}
-              />
-              <h1>mile</h1>
-              <h3>
+    <div className="App">
+      <div class="alert alert-dark login__cs" role="alert">
+        <div>
+          <span>Call Us Now: +62 812-1133-5608</span>
+          <button onClick={() => changeLanguage("en")}>en</button>
+          <button onClick={() => changeLanguage("id")}> id</button>
+        </div>
+      </div>
+      <img
+        className="login__imgBg"
+        src="https://taskdev.mile.app/d09f7f2de7d88fb5f1575273b8a26426.png"
+        alt=""
+      />
+      <Container className="m4 login__container">
+        <div className="left">
+          <div className="left__content">
+            <h1>mile</h1>
+            <h3>
+              <Trans i18nKey="description.part1">
                 The most robust Field Service Management solution, built to
                 scale for enterprise
-              </h3>
-              <button>request demo</button>
-            </div>
+              </Trans>
+            </h3>
+            <button>
+              <Trans i18nKey="description.part2">request demo </Trans>{" "}
+            </button>
           </div>
-          <div className="right">
-            <img
-              src="https://taskdev.mile.app/69ece7b1819e760b63623e810922b59e.png"
-              alt=""
-            />
-            <p className="slogan">
+        </div>
+        <div className="right">
+          <img
+            src="https://taskdev.mile.app/69ece7b1819e760b63623e810922b59e.png"
+            alt=""
+          />
+          <p className="slogan">
+            <Trans i18nKey="description.part3">
               Your one stop platform to manage all of your field service
               management
-            </p>
-            <input
-              type="text"
-              name=""
-              placeholder="Enter your organization's name"
-              id=""
-            />
-            <button>Login</button>
-            <p className="login__contactInfo">
+            </Trans>
+          </p>
+          <input
+            type="text"
+            name=""
+            placeholder="Enter your organization's name"
+            id=""
+          />
+          <button>Login</button>
+          <p className="login__contactInfo">
+            <Trans i18nKey="description.part4">
               Not registered yet? <span>Contact us </span> for more info
-            </p>
-          </div>
-        </Container>
-      </div>
-    </IntlProvider>
+            </Trans>
+          </p>
+        </div>
+      </Container>
+    </div>
   );
 }
 
